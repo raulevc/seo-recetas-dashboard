e402aa71b14fa2332a1756d31a8a38202d4f7bec
Raul E <raul82@gmail.com>
2025-07-20 12:01:10 -0500

    Primera versión del SEO Recetas Dashboard

--- seo-recetas-dashboard.php
+++ seo-recetas-dashboard.php
@@ -3,7 +3,7 @@
 Plugin Name: SEO Recetas Dashboard
 Plugin URI: https://github.com/raulevc/seo-recetas-dashboard
 Description: Plugin unificado de SEO y automatización para recetas.
-Version: 1.2.0
+Version: 1.3.0
 Author: Raúl E. Villanueva Caro
 GitHub Plugin URI: https://github.com/raulevc/seo-recetas-dashboard
 */
@@ -11,3 +11,5 @@ GitHub Plugin URI: https://github.com/raulevc/seo-recetas-dashboard
 defined('ABSPATH') or die('Sin acceso directo');
 
 require_once plugin_dir_path(__FILE__) . 'admin/dashboard.php';
+
+
